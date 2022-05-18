10b94b173b9b86e6f7571b09360cef69dd548901
Andreas Eliasson <andreaseliasson91@gmail.com>
2022-05-18 08:57:32 +0200

    initial commit

---
+++ db/__init__.py
@@ -0,0 +1,17 @@
+users = [
+    {
+        'first_name': 'Anna',
+        'last_name': 'Andersson',
+        'email': 'anna@mail.com'
+    },
+    {
+        'first_name': 'Benny',
+        'last_name': 'Berntsson',
+        'email': 'benbern@mail.com'
+    },
+    {
+        'first_name': 'Cedric',
+        'last_name': 'Cissory',
+        'email': 'cedcissy@mail.com'
+    }
+]
